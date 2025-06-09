efcc68291ac9c129f6b7bd39d2f95a06f4631627
William Ferreira da Silva <williamfdasilva@gmail.com>
2025-06-08 21:01:03 -0300

    feat(cors): allow frontend access from localhost and vercel app

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,14 @@ import { swaggerConfigEN, swaggerConfigPT } from './swagger/swagger.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      'http://localhost:3002',
+      'https://nestjs-url-shortener.vercel.app',
+    ],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
